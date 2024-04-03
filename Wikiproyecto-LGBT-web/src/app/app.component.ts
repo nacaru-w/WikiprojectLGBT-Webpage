81c2f7253312b9de45a1d15d4237cbcc969d9a62
nacaru-w <ignaciocr92@gmail.com>
2024-04-03 13:33:58 +0200

    Working on components

--- Wikiproyecto-LGBT-web/src/app/app.component.ts
+++ Wikiproyecto-LGBT-web/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbAccordionModule],
+  imports: [RouterOutlet, NgbAccordionModule, NgbAlertModule, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
