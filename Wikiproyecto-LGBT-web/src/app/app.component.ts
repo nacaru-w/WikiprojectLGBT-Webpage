996ce92afbc4474c0a7de4b4a0f8f2055032b8b6
nacaru-w <ignaciocr92@gmail.com>
2024-05-03 18:32:50 +0200

    Reorganising project

--- Wikiproyecto-LGBT-web/src/app/app.component.ts
+++ Wikiproyecto-LGBT-web/src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgbAccordionModule, NgbCarouselConfig, NgbCarouselModule, NgbCollapse, NgbScrollSpyModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, NgbAccordionModule, NgbAlertModule, NgbDropdownModule, NgbCarouselModule, NgbScrollSpyModule, CommonModule, NgbCollapse],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [NgbCarouselConfig]
 })
 export class AppComponent {
   title = 'Wikiproyecto-LGBT-web';
--- Wikiproyecto-LGBT-web/src/app/app.routes.ts
+++ Wikiproyecto-LGBT-web/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main/components/main-page/main-page.component';
 import { BlogMainComponent } from './blog/components/blog-main/blog-main.component';
 import { FormMainComponent } from './form/components/form-main/form-main.component';
+import { StatisticsMainComponent } from './statistics/components/statistics-main/statistics-main.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'blog', component: BlogMainComponent },
-    { path: 'form', component: FormMainComponent }
+    { path: 'form', component: FormMainComponent },
+    { path: 'stats', component: StatisticsMainComponent }
 ];
