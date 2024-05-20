b59eb3315ddfe0034d432cf371a717431007184d
nacaru-w <ignaciocr92@gmail.com>
2024-05-20 22:44:03 +0200

    Updated blog component

--- Wikiproyecto-LGBT-web/src/app/blog/components/blog-main/blog-main.component.ts
+++ Wikiproyecto-LGBT-web/src/app/blog/components/blog-main/blog-main.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { MediawikiService } from '../../../services/mediawiki.service';
 import { Observable } from 'rxjs';
@@ -11,50 +11,12 @@ import { Observable } from 'rxjs';
   templateUrl: './blog-main.component.html',
   styleUrl: './blog-main.component.scss'
 })
-export class BlogMainComponent implements OnInit {
+export class BlogMainComponent {
   lastThreeArticles: string[] = [];
   lastThreeTexts: string[] = [];
 
   constructor(private mediawikiService: MediawikiService) { }
 
-  ngOnInit(): void {
-    this.getLastThreeLGBTArticles();
-    this.getLastThreeLGBTExtracts();
-  }
 
-  showPageContent(title: string): void {
-    this.mediawikiService.getPageContent(title).subscribe(res => {
-      console.log(res);
-    })
-  }
-
-  assignExtracts(title: string): void {
-    this.mediawikiService.getPageExtract(title).subscribe(res => {
-      let croppedExtract = this.cropString(res, 35);
-      this.lastThreeTexts.push(croppedExtract);
-    })
-  }
-
-  getLastThreeLGBTArticles(): void {
-    this.mediawikiService.getLGBTArticleList().subscribe(res => {
-      this.lastThreeArticles = res.slice(-3);
-    });
-  }
-
-  getLastThreeLGBTExtracts(): void {
-    for (let article of this.lastThreeArticles) {
-      this.assignExtracts(article);
-    }
-  }
-
-  cropString(input: string, maxLength: number): string {
-    const words = input.split(' ');
-    const croppedWords = words.slice(0, maxLength);
-    let croppedString = croppedWords.join(' ');
-    if (words.length > maxLength) {
-      croppedString += '...';
-    }
-    return croppedString
-  }
 
 }
