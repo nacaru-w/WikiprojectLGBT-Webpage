1bb25d98df5be820775155444213993598b977c8
nacaru-w <ignaciocr92@gmail.com>
2024-06-08 11:24:08 +0200

    Finished blog edit component

--- Wikiproyecto-LGBT-web/src/app/blog/components/blog-edit/blog-edit.component.ts
+++ Wikiproyecto-LGBT-web/src/app/blog/components/blog-edit/blog-edit.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-blog-edit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './blog-edit.component.html',
   styleUrl: './blog-edit.component.scss',
 })
@@ -14,9 +14,10 @@ export class BlogEditComponent {
 
   constructor(private formBuilder: FormBuilder) {
     this.blogForm = this.formBuilder.group({
-      editorContent: new FormControl(
-        { disabled: false }
-      )
+      author: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+      date: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.maxLength(100)]),
+      content: new FormControl('', [Validators.maxLength(10000)]),
     })
   }
 
@@ -25,6 +26,23 @@ export class BlogEditComponent {
   }
 
   ngOnDestroy(): void {
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const control = this.blogForm.get(fieldName);
+    return !control!.valid && (control!.dirty)
+  }
+
+  getErrorMessage(fieldName: string): string | void {
+    const control = this.blogForm.get(fieldName);
+    if (control?.errors) {
+      switch (Object.keys(control?.errors)[0]) {
+        case 'maxlength':
+          return `El campo no puede ser mayor a ${control.errors['maxlength'].requiredLength} caracteres`
+        case 'required':
+          return 'Este campo es obligatorio'
+      }
+    }
   }
 
   onSubmit() {
