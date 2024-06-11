1fd9fc44934204f80aa6229518ae7f3efe2e8b02
nacaru-w <ignaciocr92@gmail.com>
2024-06-12 00:50:50 +0200

    Updating serve.ts

--- Wikiproyecto-LGBT-web/server.ts
+++ Wikiproyecto-LGBT-web/server.ts
@@ -4,6 +4,16 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import mysql from 'mysql2/promise';
+import { credentials } from './credentials';
+
+async function sample() {
+  const connection = await mysql.createConnection(
+    credentials
+  );
+  const [rows, _] = await connection.execute('select * FROM prueba WHERE a > ?', [3]);
+  return rows
+}
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -18,7 +28,9 @@ export function app(): express.Express {
   server.set('views', browserDistFolder);
 
   // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
+  server.get('/api/prueba', (req, res) => {
+    sample().then((rows) => res.send(rows))
+  });
   // Serve static files from /browser
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
@@ -49,7 +61,7 @@ function run(): void {
   // Start up the Node server
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Expresss server listening on http://localhost:${port}`);
   });
 }
 
