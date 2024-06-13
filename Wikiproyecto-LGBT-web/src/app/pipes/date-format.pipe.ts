bb374ff09325ade4a84aaa2ad8a7aa330109d70d
nacaru-w <ignaciocr92@gmail.com>
2024-06-13 13:56:39 +0200

    Creating pipe to format timestamps

---
+++ Wikiproyecto-LGBT-web/src/app/pipes/date-format.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat',
+  standalone: true
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: Date): string {
+    if (!value) return '';
+
+    const date = new Date(value);
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+
+    return `${month}-${day}-${year}`;
+  }
+
+}
