1cbc7dabcd45c347cfcb6409982b3d68a61d0958
nacaru-w <ignaciocr92@gmail.com>
2024-06-14 01:40:37 +0200

    Adding new routes

--- Wikiproyecto-LGBT-web/src/app/app.routes.ts
+++ Wikiproyecto-LGBT-web/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { StatisticsSharedComponent } from './statistics/components/statistics-sh
 import { BlogAdminComponent } from './blog/components/blog-admin/blog-admin.component';
 import { BlogEditComponent } from './blog/components/blog-edit/blog-edit.component';
 import { ErrorComponent } from './shared/components/error/error.component';
+import { AttributionComponent } from './legal/components/attribution/attribution.component';
+import { BlogPostComponent } from './blog/components/blog-post/blog-post.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -18,7 +20,11 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'blog-admin', component: BlogAdminComponent },
     { path: 'blog-edit', component: BlogEditComponent },
-    { path: 'privacy', component: PrivacyComponent },
+    { path: 'privacy', component: PrivacyComponent, data: { animation: ['PrivacyPage', 'footerAnimation'] } },
+    { path: 'attribution', component: AttributionComponent, data: { animation: ['AttributionPage', 'footerAnimation'] } },
+
+    { path: 'blog/:id', component: BlogPostComponent, data: { animation: ['BlogPostPage', 'footerAnimation'] } },
+    { path: 'blog-edit/:id', component: BlogEditComponent },
 
     { path: '**', pathMatch: 'full', component: ErrorComponent }
 
