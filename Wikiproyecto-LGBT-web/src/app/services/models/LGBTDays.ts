fe4e3f89496bf3a803de84d08232969dc28fcbf4
nacaru-w <ignaciocr92@gmail.com>
2024-07-16 14:25:16 +0200

    Making types more specific through union typing

---
+++ Wikiproyecto-LGBT-web/src/app/services/models/LGBTDays.ts
@@ -0,0 +1,19 @@
+export interface LGBTDays {
+    [name: string]: {
+        keyword:
+        'agender' |
+        'aro' |
+        'asexual' |
+        'bear' |
+        'bisexual' |
+        'genderqueer' |
+        'intersex' |
+        'lesbian' |
+        'lgbt' |
+        'nb' |
+        'pan' |
+        'trans',
+        month: number,
+        days: number[]
+    }
+}
