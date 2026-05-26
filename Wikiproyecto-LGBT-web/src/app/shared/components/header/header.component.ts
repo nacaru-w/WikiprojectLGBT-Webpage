ef4718d6b09c0d1cdd003923295cb25258a2275e
nacaru-w <ignaciocr92@gmail.com>
2026-05-26 21:06:13 +0200

    [UPD] Improving header links and language switcher responsiveness

--- Wikiproyecto-LGBT-web/src/app/shared/components/header/header.component.ts
+++ Wikiproyecto-LGBT-web/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 import { BarbaService } from '../../../services/barba.service';
 import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
@@ -8,7 +8,7 @@ import { TranslatePipe } from '@ngx-translate/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbCollapse, RouterLink, LanguageSwitcherComponent, TranslatePipe],
+  imports: [NgbCollapse, RouterLink, RouterLinkActive, LanguageSwitcherComponent, TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   providers: [Router]
